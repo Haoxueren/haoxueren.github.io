1975d562ffd1af66ea847de31dbcc738d2650a6e
haoxueren <haoxueren@126.com>
2020-03-02 01:03:59 +0800

    my blog reupload

--- README.md
+++
@@ -1,1 +0,0 @@
-# haoxueren.github.io
---
+++ 复制到public目录下运行.sh
@@ -0,0 +1,10 @@
+
+git init 
+
+git remote add origin https://github.com/haoxueren/haoxueren.github.io.git
+
+git add --all
+
+git commit --message "¸üÐÂ²©¿Í"
+
+git push origin master --force
